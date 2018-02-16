8af7f0cbf597f5fdf35da7620bedd19f2cb8f44d
Marcos Medeiros <mhlmedeiros@gmail.com>
2018-02-16 01:01:10 -0200

    Salva dados em hdf5
    	-> script separado para salvar dados;
    	-> a intenção é que sirva para outros sistemas;

--- gaussian_package_1D.jl
+++ gaussian_package_1D.jl
@@ -5,8 +5,8 @@
 #
 #
 #*******************************************************************************
-using PyPlot;
-using HDF5;
+
+include("save_the_data.jl")
 
 PyPlot.matplotlib[:rc]("text", usetex=true) # allow tex rendering
 PyPlot.matplotlib[:rc]("font", family="serif")
@@ -21,25 +21,28 @@ hbar = 0.65821188926; # meV.ps
 # definição da rede espacial
 L = 1000;
 Nx = 2*L;
-x = linspace(-L/2, L/2, Nx+1)
-x = x[1:(end-1)];
-Δx = x[2] - x[1];
+x_grid = linspace(-L/2, L/2, Nx+1)
+x_grid = x_grid[1:(end-1)];
+Δx = x_grid[2] - x_grid[1];
 
 # definição da rede do "espaço-recíproco"
 k = fftshift(linspace(-pi/Δx, pi/Δx, Nx+1)[1:end-1]);
 Δk = k[2]-k[1];
 
-# cond. inicial
-σ = 15 ; # L/100
-k0_max = pi/(2*Δx);
-k₀ = 0.01 * k0_max;  # o valor de "k0" não pode ultrapassar π/(2dx)
+# Condição inicial
+σ = 15 ;
+k₀_max = pi/(2*Δx);
+frac_max = 0.01; # fração do valor máximo de "k" que será usado na cond. inicial
+k₀ = frac_max * k₀_max;  # o valor de "k₀" não pode ultrapassar π/(2*Δx)
 x₀ = -200;
-Ψ_0 = exp(-(x-x_0).^2/(2*σ^2) + 1im*k₀*x); # Gaussian packet
+Ψ₀ = exp(-(x_grid-x₀).^2/(2*σ^2) + 1im*k₀*x_grid); # Gaussian packet
+
 
 # Hamiltoniano separado em "T + V"
-α, β, γ = 10, 0.07, 0;
 T_k = h2m * k.^2;
-V_x = α./(e.^(-β*(x-γ))+1);
+
+α, β, γ = 10, 0.07, 0;
+V_x = α./(e.^(-β*(x_grid-γ))+1);
 
 
 function psi_dt(ψ,Δt)
@@ -55,7 +58,6 @@ function psi_dt(ψ,Δt)
   return psi;
 end
 
-
 function psit(Ψ; t_final = 10.0, N_steps = 100)
   tspan = linspace(0, t_final, N_steps);
   delta_t = tspan[2]-tspan[1];
@@ -67,105 +69,21 @@ function psit(Ψ; t_final = 10.0, N_steps = 100)
   Ψ
 end
 
+t_f = 2.5;
+ψ_1 = psit(Ψ_0, t_final = t_f, N_steps = 10);
+nome_dados = string("psi_t_",round(t_f,2))
 
-t_f = 1.0;
-ψ_1 = psit(Ψ_0, t_final = t_f, N_steps = 10)
-# plot(x,abs(ψ_1).^2)
-# plot(x,0.5*10.0^(-2)*V_x, linewidth=2.0);
-# # ylim(-0.05,1.05);
-# # xlim(-20,20)
-# title("Gaussian distribution");
-# xlabel(L"$x$");
-# ylabel(L"$|\psi(x)|^2$");
+nome_arquivo = "dados_split_operator.h5";
+nome_grupo = "split_1D";
+nome_subg = "potencial_degrau";
 
+atr_cond_ini = Dict("largura_pacote" => round(σ,1),
+                        "x_0" => round(x₀,1),
+                        "frac_k_max" => round(frac_max,3));
 
-
-
-
-nome_dados_real = string("psi_t_",round(t_f,2),"_real")
-nome_dados_imag = string("psi_t_",round(t_f,2),"_imag")
-
-h5open("dados_split_operator.h5","w") do file
-    # Cria os grupos, ou abre aqueles já existentes:
-    try
-        g = g_create(file, "split_1D");
-        h = g_create(g, "potencial_degrau");
-    catch
-        g = file["split_1D"];
-        h = g["potencial_degrau"];
-    end
-
-    # Caso não tenha salvo o potencial, salve-o:
-    try
-        h["Potencial"] = V_x;
-        attrs(h["Potencial"])["alpha"] = string(α);
-        attrs(h["Potencial"])["beta"] = string(β);
-        attrs(h["Potencial"])["gamma"] = string(γ);
-    end
-
-    # Caso não tenha salvo a condição inicial, salve-a:
-    try
-        h["Cond_Ini_real"] = real(Ψ_0);
-        h["Cond_Ini_imag"] = imag(Ψ_0);
-        h["grid"] = x;
-        attrs(g["potencial_degrau"])["x_0"] = string(round(x₀,1));
-        attrs(g["potencial_degrau"])["k_0"] = string(round(k₀,1));
-        attrs(g["potencial_degrau"])["sigma"] = string(round(σ,1));
-    end
-
-    # Caso não tenha salvo a solução para o t_f corrente, salve-a:
-    try
-        h[nome_dados_real] = real(ψ_1);
-        h[nome_dados_imag] = imag(ψ_1);
-    end
-end
+atr_pot = Dict("altura" => round(α,1),
+                        "beta" => round(β,2),
+                        "posição" => round(γ,1));
 
 
-
-
-
-primeira_camada = h5open("dados_split_operator.h5","r") do file
-    names(file)
-end
-#
-#
-#
-segunda_camada = h5open("dados_split_operator.h5","r") do file
-    segunda_camada = [];
-    for name in primeira_camada
-        append!(segunda_camada,names(file[name]))
-    end
-    return segunda_camada
-end
-#
-
-terceira_camada = h5open("dados_split_operator.h5","r") do file
-    terceira_camada = [];
-    for name in primeira_camada
-        g = file[name];
-        for name_grupo in segunda_camada
-            append!(terceira_camada,names(g[name_grupo]))
-        end
-    end
-    return terceira_camada
-end
-
-#
-Atr_grupo = h5readattr("dados_split_operator.h5", "split_1D/potencial_degrau")
-#
-#
-#
-# h5writeattr("dados_split_operator.h5", "split_1D/potencial_degrau",
-#         Dict( "x_0" => string(round(x₀,1)),
-#               "k_0" => string(round(k₀,1)),
-#               "sigma" => string(round(σ,1))));
-#
-h5readattr("dados_split_operator.h5","split_1D/potencial_degrau/")
-#
-# name_path_real = string("split_1D/potencial_degrau/",nome_dados_real)
-# name_path_imag = string("split_1D/potencial_degrau/",nome_dados_imag)
-#
-# psi_lido_real = h5read("dados_split_operator.h5",name_path_real)
-# psi_lido_imag = h5read("dados_split_operator.h5",name_path_imag)
-#
-# psi_lido_total = complex(psi_lido_real,psi_lido_imag)
+save_them_all(nome_arquivo, nome_grupo, nome_subg, ψ_1, nome_dados, atr_cond_ini,  atr_pot)
--- save_the_data.jl
+++ save_the_data.jl
@@ -1,18 +1,58 @@
 using HDF5;
 
-function save_data(name_file, name_group, name_sub_group)
+function save_all(h, g, subg_name, ψ, data_name, ini_cond_attr,  pot_attr)
+    save_potential(h, pot_attr)
+    save_cond_ini(h, g, subg_name, ini_cond_attr)
+    save_function(h, ψ, data_name)
+end
 
-    # Criar as coisas: Arquivo, Grupo, SubGrupo
+function save_potential(h, attr_dict)
+    h["Potencial"] = V_x;
+    for (key, value) in attr_dict
+        # Os atributos do Potencial são dados pelas tuplas (key, value) do
+        # dictionary "attr_dict".
+        attrs(h["Potencial"])[key] = string(value);
+    end
+end
 
-    h5open(name_file,"w") do file
-        # Cria, ou abre o grupo e o subgrupo:
-        try g = g_create(file, name_group) catch g = g_open(file, name_group) end;
-        try h = g_create(g, name_sub_group) catch h = g_open(file, name_sub_group) end;
+function save_cond_ini(h, g, subg_name, attr_dict)
+    # Salva a condição inicial:
+    h["Cond_Ini_real"] = real(Ψ_0);
+    h["Cond_Ini_imag"] = imag(Ψ_0);
+    h["grid"] = collect(x_grid);
+    for (key, value) in attr_dict
+        attrs(g[subg_name])[key] = string(value);
+    end
+end
 
-        # Caso não tenha salvo o potencial, salve-o:
-        try
-            h["Potencial"] = V_x;
-            attrs(h["Potencial"])["alpha"] = string(α);
-            attrs(h["Potencial"])["beta"] = string(β);
-            attrs(h["Potencial"])["gamma"] = string(γ);
+function save_function(handle, ψ, data_name)
+    # Salva a solução para o tempo final:
+    nome_real = string(data_name,"_real");
+    nome_imag = string(data_name,"_imag");
+    handle[nome_real] = real(ψ);
+    handle[nome_imag] = imag(ψ);
+end
+
+function save_them_all(file_name, group_name, subg_name, ψ, data_name, ini_cond_attr,  pot_attr)
+    try
+        h5open(file_name,"r+") do file
+            # Cria os grupos, ou abre aqueles já existentes:
+            try
+                g = file[group_name];
+                h = g[ subg_name];
+                save_function(h, ψ, data_name)
+            catch
+                g = g_create(file, group_name);
+                h = g_create(g, subg_name);
+                save_all(h, g, subg_name, ψ, data_name, ini_cond_attr,  pot_attr)
+            end
         end
+    catch
+        h5open(file_name,"w") do file
+            # Cria os grupos
+            g = g_create(file, group_name);
+            h = g_create(g, subg_name);
+            save_all(h, g, subg_name, ψ, data_name, ini_cond_attr,  pot_attr)
+        end
+    end
+end
